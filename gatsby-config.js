5b1080dc5cf85ebed277e1a272f0468a86f33984
Patrick Franz <clymbing@gmail.com>
2021-01-24 13:07:23 -0800

    add google analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -42,14 +42,14 @@ module.exports = {
       options: {
         // You can add multiple tracking ids and a pageview event will be fired for all of them.
         trackingIds: [
-          'UA-172462366-3', // Google Analytics / GA
+          'G-E32QDMG1J2', // Google Analytics / GA
           // "AW-CONVERSION_ID", // Google Ads / Adwords / AW
           // "DC-FLOODIGHT_ID", // Marketing Platform advertising products (Display & Video 360, Search Ads 360, and Campaign Manager)
         ],
         // This object gets passed directly to the gtag config command
         // This config will be shared across all trackingIds
         gtagConfig: {
-          optimize_id: 'OPT_CONTAINER_ID',
+          optimize_id: 'G-E32QDMG1J2',
           anonymize_ip: true,
           cookie_expires: 0,
         },
